ea43b8bfd941268fc09e37c8d808d170e60260b0
rinrab <tima@chemodax.net>
2024-03-12 22:06:37 +0100

    Merge MeasureProcessCmdlet.h into MeasureProcessCmdlet.cpp

--- src/pbench.PowerShell/MeasureProcessCmdlet.cpp
+++ src/pbench.PowerShell/MeasureProcessCmdlet.cpp
@@ -1,19 +1,47 @@
-#include "MeasureProcessCmdlet.h"
 #include "ProcessStats.h"
 
-void MeasureProcessCmdlet::ProcessRecord()
-{
-    String^ str = gcnew String(filePath);
-    for each (String^ arg in argumentList)
-    {
-        str += " ";
-        str += arg;
-    }
-    WriteVerbose(str);
+using namespace System;
+using namespace System::Management::Automation;
 
-    ProcessStats^ obj = gcnew ProcessStats();
-    obj->RealTime = gcnew TimeSpan(0, 0, 3);
-    obj->CpuTime = gcnew TimeSpan(0, 0, 1);
-    obj->ReadCount = 123;
-    WriteObject(obj);
-}
+[Cmdlet("Measure", "Process")]
+[Alias("pbench")]
+[OutputType(ProcessStats::typeid)]
+public ref class MeasureProcessCmdlet : PSCmdlet
+{
+public:
+    [Parameter(Position = 0, Mandatory = true)]
+        property System::String^ FilePath
+    {
+        System::String^ get() { return filePath; }
+        void set(String^ value) { filePath = value; }
+    }
+
+    [Parameter(ValueFromRemainingArguments = true, Mandatory = false)]
+        property array<String^>^ ArgumentList
+    {
+        array<String^>^ get() { return argumentList; }
+        void set(array<String^>^ value) { argumentList = value; }
+    }
+
+protected:
+    void ProcessRecord() override
+    {
+        String^ str = gcnew String(filePath);
+        for each (String ^ arg in argumentList)
+        {
+            str += " ";
+            str += arg;
+        }
+        WriteVerbose(str);
+
+        ProcessStats^ obj = gcnew ProcessStats();
+        obj->RealTime = gcnew TimeSpan(0, 0, 3);
+        obj->CpuTime = gcnew TimeSpan(0, 0, 1);
+        obj->ReadCount = 123;
+        WriteObject(obj);
+    }
+
+private:
+    String^ filePath;
+    array<String^>^ argumentList;
+};
