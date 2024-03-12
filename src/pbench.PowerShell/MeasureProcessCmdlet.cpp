13cd2fadbda8bb6f00fbea0dc585934891da3abd
rinrab <tima@chemodax.net>
2024-03-12 21:11:17 +0100

    Move class declaration to MeasureProcessCmdlet.h file.

--- src/pbench.PowerShell/MeasureProcessCmdlet.cpp
+++ src/pbench.PowerShell/MeasureProcessCmdlet.cpp
@@ -1,32 +1,4 @@
-using namespace System;
-using namespace System::Management::Automation;
-
-[Cmdlet("Measure", "Process")]
-[Alias("pbench")]
-public ref class MeasureProcessCmdlet : PSCmdlet
-{
-public:
-    [Parameter(Position = 0, Mandatory = true)]
-    property System::String^ FilePath
-    {
-        System::String^ get() { return filePath; }
-        void set(String^ value) { filePath = value; }
-    }
-
-    [Parameter(ValueFromRemainingArguments = true)]
-    property array<String^>^ ArgumentList
-    {
-        array<String^>^ get() { return argumentList; }
-        void set(array<String^>^ value) { argumentList = value; }
-    }
-
-protected:
-    virtual void ProcessRecord() override;
-
-private:
-    String^ filePath;
-    array<String^>^ argumentList;
-};
+#include "MeasureProcessCmdlet.h"
 
 void MeasureProcessCmdlet::ProcessRecord()
 {
